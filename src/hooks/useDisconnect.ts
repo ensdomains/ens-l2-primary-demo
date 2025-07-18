f8dfd659c8c5179ec455c77f762e1ab74c24dac4
storywithoutend <storywithoutend@gmail.com>
2025-07-18 15:12:18 +0800

    Clean up useDisconnect code

--- src/hooks/useDisconnect.ts
+++ src/hooks/useDisconnect.ts
@@ -1,26 +1,11 @@
-import { useConfig } from "wagmi"
-import { disconnect as disconnectWagmi } from "wagmi/actions"
 import { useDisconnect as useDisconnectWagmi } from "wagmi"
 
 const LOCAL_STORAGE_KEYS = ['rk-latest-id', 'wagmi.recentConnectorId', 'wagmi.store']
 
 export const useDisconnect = () => {
-  const { disconnect: disconnectWagmi } = useDisconnectWagmi({ mutation: { onSettled: () => {
-    console.log("disconnecting")
+  return useDisconnectWagmi({ mutation: { onSettled: () => {
     LOCAL_STORAGE_KEYS.forEach(key => {
       localStorage.removeItem(key)
     })
   }}})
-  const config = useConfig()
-  const disconnect = async () => {
-    await disconnectWagmi(config)
-    LOCAL_STORAGE_KEYS.forEach(key => {
-      localStorage.removeItem(key)
-    })
-  }
-
-  return {
-    disconnect: disconnectWagmi,
-  }
-  
 }
