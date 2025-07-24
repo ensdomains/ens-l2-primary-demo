e6af7d281c75b76f159d74c9dcc67291b95769fe
storywithoutend <storywithoutend@gmail.com>
2025-07-25 00:25:52 +0800

    Add 0 delay to header button reappearing so that it can position dropdown correctly

--- src/components/atoms/WalletConnectButton.tsx
+++ src/components/atoms/WalletConnectButton.tsx
@@ -7,6 +7,21 @@ import { useResolvedPrimaryName } from "@/hooks/useResolvedPrimaryName"
 import { useZorb } from "@/hooks/useZorb/useZorb"
 import { useEnsAvatar } from "@/hooks/useEnsAvatar"
 import { useTransactionStore } from "@/stores/transactionStore"
+import { useEffect, useState } from "react"
+
+// NOTE: We will hide the button if there is a view open so that if the button needs to be re-rendered,
+// the dropdwon will be properly positioned. We also need to debounce the state change so that the modal is
+// gone before the button is rendered.
+const useIsHeaderVisible = () => {
+  const { getCurrentView } = useTransactionStore()
+  const { view } = getCurrentView()
+  const [isVisible, setIsVisible] = useState(!view)
+  useEffect(() => {
+    if (!view) setTimeout(() => setIsVisible(true), 0)
+    else setIsVisible(false)
+  }, [view])
+  return isVisible
+}
 
 export const WalletConnectButton = () => {
   const { address, chainId } = useAccount()
@@ -16,29 +31,35 @@ export const WalletConnectButton = () => {
     coinType: chains.find((chain) => chain.id === chainId)?.coinType ?? 60,
   })
 
-  // NOTE: We will hide the button if there is a view open so that if the button needs to be re-rendered, 
-  // the dropdwon will be properly positioned.
-  const { getCurrentView } = useTransactionStore()
-  const { view } = getCurrentView()
-
   const { data: avatar } = useEnsAvatar({
     name: primaryName,
   })
 
   const zorb = useZorb(address ?? "")
 
+  const isVisible = useIsHeaderVisible()
+
   const { openConnectModal } = useConnectModal()
 
-  if (!!view) return null
+  if (!isVisible) return null
   if (address) {
-    return <Profile address={address} ensName={primaryName} avatar={avatar ?? zorb} dropdownItems={[{
-      label: 'Disconnect',
-      icon: ExitSVG,
-      color: 'redPrimary',
-      onClick: () => {
-        disconnect()
-      }
-    }]}/>
+    return (
+      <Profile
+        address={address}
+        ensName={primaryName}
+        avatar={avatar ?? zorb}
+        dropdownItems={[
+          {
+            label: "Disconnect",
+            icon: ExitSVG,
+            color: "redPrimary",
+            onClick: () => {
+              disconnect()
+            },
+          },
+        ]}
+      />
+    )
   }
   return (
     <Button
